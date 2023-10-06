78800cf392778d3b99227baac7daea80e83a8a00
RakibR7 <G00408644@ATU.IE>
2023-10-06 11:29:56 +0100

    fixed lab1

--- src/main/java/ie/atu/requestcontrollerlab2/RequestController.java
+++ src/main/java/ie/atu/requestcontrollerlab2/RequestController.java
@@ -7,20 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
-
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
-    }
-
-    @GetMapping("/greet/{name}")
-    public String greetByName(@PathVariable String name){
-        return "Hello: " + name;
-    }
-
-    @GetMapping("/details")
-    public String details(@RequestParam int age, String name) {
-        return "age, name" + age + name;
     }
 }
 
