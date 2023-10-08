5cd34962824862de6d41b4d44518366dd1c1186a
RakibR7 <G00408644@ATU.IE>
2023-10-08 21:42:18 +0100

    Addition is working as intended

--- src/main/java/ie/atu/requestcontrollerlab2/Calculate.java
+++ src/main/java/ie/atu/requestcontrollerlab2/Calculate.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-public class CalculatorController  {
-    //@GetMapping("/calculate")
-    public int add(@RequestParam int num1, int num2){
-        return num1+num2;
+public class Calculate{
+    public int add(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation){
+        int add = num1+num2;
+        return add;
     }
 }
+
--- src/main/java/ie/atu/requestcontrollerlab2/RequestController.java
+++ src/main/java/ie/atu/requestcontrollerlab2/RequestController.java
@@ -1,13 +1,13 @@
 package ie.atu.requestcontrollerlab2;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
-
+    /*
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
@@ -22,10 +22,10 @@ public class RequestController {
     public String details(@RequestParam int age, String name) {
         return "age, name" + age + name;
     }
-
+    */
     @GetMapping("/calculate")
-    public String calculate(@RequestParam int num1, int num2) {
-        return "num1, num2" + num1 + num2;
+    public String calculate(@RequestParam int num1, @RequestParam int num2) {
+        return "Num1: "+num1+" Num2: "+num2;
     }
 }
 
