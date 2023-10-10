f090a4fb8f2a087ecd1f20089352d872dced8ac5
RakibR7 <G00408644@ATU.IE>
2023-10-10 10:35:32 +0100

    removed unused operators, now it looks better.

--- src/main/java/ie/atu/requestcontrollerlab2/CalculatorController.java
+++ src/main/java/ie/atu/requestcontrollerlab2/CalculatorController.java
@@ -9,24 +9,19 @@ public class CalculatorController {
     @GetMapping("/calculate") //path needed for url
     public Results calculate(@RequestParam int num1, @RequestParam int num2,  @RequestParam String operation) {     //req param is about binding to html
         int total = 0;
-        String operator;
 
         switch(operation){
             case "add":
                 total = num1+num2;
-                operator = "add";
                 break;
             case "subtract":
                 total = num1-num2;
-                operator = "subtract";
                 break;
             case "multiply":
                 total = num1*num2;
-                operator = "multiply";
                 break;
             case "divide":
                 total = num1/num2;
-                operator = "divide";
                 break;
         }
         return new Results(operation, total);
